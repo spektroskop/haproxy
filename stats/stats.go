f33933ab1b0e1c61effcba7ea502385d5cb6496c
Alexander Stensrud <alex@stensrud.nu>
2016-09-09 10:50:41 +0200

    Remove empty comments

--- stats/stats.go
+++ stats/stats.go
@@ -10,7 +10,6 @@ import (
 	"time"
 )
 
-// SetServiceMapper ..
 func SetServiceMapper(m func(Service) Service) {
 	if m != nil {
 		serviceMapper = m
@@ -21,7 +20,6 @@ var serviceMapper = func(svc Service) Service {
 	return svc
 }
 
-// SetMapper ..
 func SetMapper(m func([]Service) []Service) {
 	if m != nil {
 		mapper = m
@@ -32,54 +30,43 @@ var mapper = func(svc []Service) []Service {
 	return svc
 }
 
-// Service ..
 type Service map[string]interface{}
 
-// String ..
 func (svc Service) String(key string) (v string) {
 	v, _ = svc[key].(string)
 	return
 }
 
-// Int ..
 func (svc Service) Int(key string) (v int64) {
 	v, _ = svc[key].(int64)
 	return
 }
 
-// Field ..
 type Field interface {
 	Name() string
 	Value(string) (interface{}, error)
 }
 
-// TextField ..
 type TextField string
 
-// Name ..
 func (f TextField) Name() string {
 	return string(f)
 }
 
-// Value ..
 func (f TextField) Value(v string) (interface{}, error) {
 	return v, nil
 }
 
-// NumberField ..
 type NumberField string
 
-// Name ..
 func (f NumberField) Name() string {
 	return string(f)
 }
 
-// Value ..
 func (f NumberField) Value(v string) (interface{}, error) {
 	return strconv.ParseInt(v, 10, 64)
 }
 
-// FieldMap ..
 type FieldMap map[string]Field
 
 var fields = FieldMap{
@@ -87,14 +74,12 @@ var fields = FieldMap{
 	"pxname": TextField("pxname"),
 }
 
-// SetFields ..
 func SetFields(m FieldMap) {
 	if m != nil {
 		fields = m
 	}
 }
 
-// New ..
 func New(mode, address string, timeout time.Duration) ([]Service, error) {
 	conn, err := net.Dial(mode, address)
 	if err != nil {
@@ -117,7 +102,6 @@ func FromFile(name string) ([]Service, error) {
 	return FromReader(reader)
 }
 
-// FromReader ..
 func FromReader(reader io.Reader) ([]Service, error) {
 	reader.Read(make([]byte, 2))
 	cr := csv.NewReader(reader)
